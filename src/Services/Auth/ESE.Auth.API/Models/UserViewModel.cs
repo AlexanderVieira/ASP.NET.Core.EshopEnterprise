0a0b265190707b57fd4ff04d5499cf2fe7f82b56
Alexander Silva <asilva943rj@gmail.com>
2020-11-14 19:15:55 -0300

    ConnectionString para MSSQLLocalDB no visual studio 2019

--- src/Services/Auth/ESE.Auth.API/Models/UserViewModel.cs
+++ src/Services/Auth/ESE.Auth.API/Models/UserViewModel.cs
@@ -13,7 +13,7 @@ namespace ESE.Auth.API.Models
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
         public string Password { get; set; }
 
-        [Compare("Senha", ErrorMessage = "As senhas não conferem.")]
+        [Compare("Password", ErrorMessage = "As senhas não conferem.")]
         public string PasswordConfirmation { get; set; }
     }
 
