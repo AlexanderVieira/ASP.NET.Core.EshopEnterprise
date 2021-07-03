2210d11fa24190fa873fda41725f1b6a4307936d
Alexander Silva <asilva943rj@gmail.com>
2021-07-03 00:07:13 -0300

    Implementação serviço de autenticação, bem como endpoint para refresh do token na api Auth

--- src/Services/Auth/ESE.Auth.API/Configuration/IdentityConfig.cs
+++ src/Services/Auth/ESE.Auth.API/Configuration/IdentityConfig.cs
@@ -5,12 +5,19 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NetDevPack.Security.JwtSigningCredentials;
 
 namespace ESE.Auth.API.Configuration
 {
     public static class IdentityConfig
     {
         public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration){
+
+            var appSettingsSection = configuration.GetSection("AppTokenSettings");
+            services.Configure<AppTokenSettings>(appSettingsSection);
+
+            services.AddJwksManager(options => options.Algorithm = Algorithm.ES256)
+                .PersistKeysToDatabaseStore<AuthDbContext>();
 
             services.AddDbContext<AuthDbContext>(options => 
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
--- src/Services/Auth/ESE.Auth.API/Data/AuthDbContext.cs
+++ src/Services/Auth/ESE.Auth.API/Data/AuthDbContext.cs
@@ -1,10 +1,15 @@
+using ESE.Auth.API.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using NetDevPack.Security.JwtSigningCredentials;
+using NetDevPack.Security.JwtSigningCredentials.Store.EntityFrameworkCore;
 
 namespace ESE.Auth.API.Data
 {
-    public class AuthDbContext: IdentityDbContext
+    public class AuthDbContext: IdentityDbContext, ISecurityKeyContext
     {
+        public DbSet<SecurityKeyWithPrivate> SecurityKeys { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
         public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
     }
 }
