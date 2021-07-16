3303c9db9ab6b9a362e7afc40ece00dfea5888a6
Alexander Silva <asilva943rj@gmail.com>
2021-07-16 19:13:54 -0300

    Testando CI/CD azure pipeline.

--- src/Services/Catalog/ESE.Catalog.API/Configuration/ApiConfig.cs
+++ src/Services/Catalog/ESE.Catalog.API/Configuration/ApiConfig.cs
@@ -14,7 +14,7 @@ namespace ESE.Catalog.API.Configuration
         public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<CatalogContext>(opt =>
-                opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+                opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), providerOptions => providerOptions.EnableRetryOnFailure()));
 
             services.AddControllers();
 
